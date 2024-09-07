a5c010b1be2be572508c6f0591d6d4499bad9c56
olusegunajibola <olusegunajibola.e@gmail.com>
2024-09-07 08:29:14 +0100

    rename files

--- B_mini_sample/sample_mini_b.py
+++ B_mini_sample/sample_mini_b.py
@@ -1,6 +1,6 @@
-# python 02_sentiment_analysis_agent.py
-# python 03_action_agents.py
-# python 04_cordinator_agent.py
+# python b_sentiment_analysis_agent.py
+# python c_action_agents.py
+# python d_cordinator_agent.py
 #
 #
 # python positive_action_agent.py
