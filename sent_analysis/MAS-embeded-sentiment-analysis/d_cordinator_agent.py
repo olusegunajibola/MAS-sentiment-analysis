0cc127844e262daf2c766c08fdc7026764228ede
olusegunajibola <olusegunajibola.e@gmail.com>
2024-10-01 19:00:01 +0100

    enable user input for cordinator agent

--- sent_analysis/MAS-embeded-sentiment-analysis/d_cordinator_agent.py
+++ sent_analysis/MAS-embeded-sentiment-analysis/d_cordinator_agent.py
@@ -6,8 +6,9 @@ def main():
 
     # Sample text data
     # text = "The company's profits are expected to rise this quarter."
-    text = "Earnings are down by half this year. The losses are very much."
-
+    # text = "Earnings are down by half this year. The losses are very much."
+    print('Enter a sentence: ')
+    text = input()
     # Process the text and send to RabbitMQ
     agent.process_and_send(text)
 
